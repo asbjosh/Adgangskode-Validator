306746b99226e22cda22a39e0f58e93c06db63a3
asbjosh <asbjornsh1@gmail.com>
2025-04-08 13:37:21 +0200

    feat: base program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿bool passwordSecure = true;
+string password;
+
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Adgangskoden skal mindst have 2 tal, et af de følgende special tegn ('!', '@', '#', '$', '%', '&', '*')");
+    Console.WriteLine("Længden skal mindst være på 8 tegn");
+    Console.Write("Indast dit nye kodeord: ");
+    password = Console.ReadLine();
+
+    passwordSecure = false;
+
+    if(password.Length < 2)
+    {
+        Console.WriteLine();
+    }
+
+} while(passwordSecure);
